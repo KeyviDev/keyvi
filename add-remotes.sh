fb183aadec50b33833c80b3bc1769c65c1cb8107
Hendrik Muhs <hendrik.muhs@gmail.com>
2015-07-17 13:16:24 +0200

    add script for remotes

---
+++ add-remotes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git remote add keyvi git@github.com:cliqz/keyvi.git
+git remote add rapidjson git@github.com:miloyip/rapidjson.git
+git remote add msgpack-c git@github.com:msgpack/msgpack-c.git
+git remote add tpie git@github.com:thomasmoelhave/tpie.git
+git remote add mprpc git@github.com:hendrik-cliqz/mprpc.git
+
