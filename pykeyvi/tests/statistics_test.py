aaea275b3949dfc2333ea08bb486726dd5e2765d
Dávid Márk Nemeskey <david@cliqz.com>
2015-09-10 18:02:51 +0200

    Deleting the dictionary file in a Windows-compatible manner.

--- pykeyvi/tests/statistics_test.py
+++ pykeyvi/tests/statistics_test.py
@@ -1,44 +1,44 @@
 # -*- coding: utf-8 -*-
 
+import contextlib
 import os
 
 import pykeyvi
+
+@contextlib.contextmanager
+def tmp_dictionary(compiler, file_name):
+    compiler.Compile()
+    compiler.WriteToFile(file_name)
+    d = pykeyvi.Dictionary(file_name)
+    yield d
+    del d
+    os.remove(file_name)
 
 def size_test():
     c = pykeyvi.KeyOnlyDictionaryCompiler()
     c.Add("Leela")
     c.Add("Kif")
-    c.Compile()
-    c.WriteToFile('brannigan_1.kv')
-    d = pykeyvi.Dictionary('brannigan_1.kv')
-    os.remove('brannigan_1.kv')
-    assert len(d) == 2
+    with tmp_dictionary(c, 'brannigan_size.kv') as d:
+        assert len(d) == 2
 
 def manifest_test():
     c = pykeyvi.KeyOnlyDictionaryCompiler()
     c.Add("Leela")
     c.Add("Kif")
-    c.Compile()
     c.SetManifest({"author": "Zapp Brannigan"})
-    c.WriteToFile('brannigan_2.kv')
-    d = pykeyvi.Dictionary('brannigan_2.kv')
-    os.remove('brannigan_2.kv')
-    m = d.GetManifest()
-    assert m['author'] == "Zapp Brannigan"
+    with tmp_dictionary(c, 'brannigan_manifest.kv') as d:
+        m = d.GetManifest()
+        assert m['author'] == "Zapp Brannigan"
 
 def statistics_test():
     c = pykeyvi.KeyOnlyDictionaryCompiler()
     c.Add("Leela")
     c.Add("Kif")
-    c.Compile()
     c.SetManifest({"author": "Zapp Brannigan"})
-    c.WriteToFile('brannigan_3.kv')
-    d = pykeyvi.Dictionary('brannigan_3.kv')
-    os.remove('brannigan_3.kv')
-    stats = d.GetStatistics()
-    gen = stats.get('General', {})
-    man = gen.get('manifest', {})
-    size = int(gen.get('number_of_keys', 0))
-
-    assert size == 2
-    assert man.get('author') == "Zapp Brannigan"
+    with tmp_dictionary(c, 'brannigan_statistics.kv') as d:
+        stats = d.GetStatistics()
+        gen = stats.get('General', {})
+        man = gen.get('manifest', {})
+        size = int(gen.get('number_of_keys', 0))
+        assert size == 2
+        assert man.get('author') == "Zapp Brannigan"
