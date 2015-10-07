806c46ce9aa573d681519ae32258e2c3372c64b0
Dávid Márk Nemeskey <david@cliqz.com>
2015-10-07 18:25:55 +0200

    JsonValueStore to write the compression-related keys to the property store.

--- keyvi/src/cpp/dictionary/fsa/internal/json_value_store.h
+++ keyvi/src/cpp/dictionary/fsa/internal/json_value_store.h
@@ -275,6 +275,8 @@ class JsonValueStore final : public IValueStoreWriter {
           pt.put("size", std::to_string(values_buffer_size_));
           pt.put("values", std::to_string(number_of_values_));
           pt.put("unique_values", std::to_string(number_of_unique_values_));
+          pt.put(std::string("__") + COMPRESSOR_KEY, compressor_->name());
+          pt.put(std::string("__") + COMPRESSION_THRESHOLD_KEY, compression_threshold_);
 
           internal::SerializationUtils::WriteJsonRecord(stream, pt);
           TRACE("Wrote JSON header, stream at %d", stream.tellp());
