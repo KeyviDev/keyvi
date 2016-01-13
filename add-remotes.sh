f1fbf05600e73bbd00704eacca8c33ec78b4e46b
Hendrik Muhs <hendrik.muhs@gmail.com>
2016-01-13 22:25:26 +0100

    add xchange 3rdparty repo

--- add-remotes.sh
+++ add-remotes.sh
@@ -4,4 +4,4 @@ git remote add rapidjson git@github.com:miloyip/rapidjson.git
 git remote add msgpack-c git@github.com:msgpack/msgpack-c.git
 git remote add tpie git@github.com:thomasmoelhave/tpie.git
 git remote add mprpc git@github.com:hendrik-cliqz/mprpc.git
-
+git remote add xchange git@github.com:xpol/xchange.git
