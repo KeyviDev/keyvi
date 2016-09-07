145bb2fd67146a1cafcdb495033f6167563f8fbf
Hendrik Muhs <hendrik.muhs@gmail.com>
2016-09-07 10:44:53 +0200

    simplify, avoid 2 returns

--- keyvi/src/cpp/dictionary/dictionary_compiler.h
+++ keyvi/src/cpp/dictionary/dictionary_compiler.h
@@ -163,12 +163,7 @@ class DictionaryCompiler
       CreateGenerator();
 
       // check that at least 1 item is there
-      if (!sorter_.can_pull()) {
-        generator_->CloseFeeding();
-        return;
-      }
-
-      {
+      if (sorter_.can_pull()) {
         number_of_items_ = sorter_.item_count();
 
         callback_trigger_ = 1+(number_of_items_-1)/100;
