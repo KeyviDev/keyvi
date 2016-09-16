d5fb56be31226ee0e326b739f1b4ce9aacf875da
Narek Gharibyan <narekgharibyan@gmail.com>
2016-09-16 13:15:25 +0200

    added script for building manylinux wheels

---
+++ build_wheels.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ev
+
+cd /keyvi/pykeyvi/
+
+PYBIN=/opt/python/cp27-cp27mu/bin
+
+${PYBIN}/python setup.py bdist_wheel
+
+# Bundle external shared libraries into the wheels
+for whl in dist/*.whl; do
+    auditwheel repair $whl -w wheelhouse/
+done
+
+#${PYBIN}/pip install pytest
+#${PYBIN}/pip install python_keyvi -f wheelhouse/
+#${PYBIN}/py.test tests/
