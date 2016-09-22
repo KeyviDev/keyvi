1dae6ac9739e0ebb2d0e7787d4058311a4a8117c
Narek Gharibyan <narekgharibyan@gmail.com>
2016-09-22 11:51:45 +0200

    minor

--- travis/build_manylinux_package.sh
+++ travis/build_manylinux_package.sh
@@ -4,4 +4,4 @@ set -ev
 docker run --rm -v `pwd`:/keyvi $DOCKER_IMAGE /keyvi/travis/build_wheels.sh
 
 pip install --user pykeyvi/wheelhouse/python_keyvi*.whl
-py.test tests/
+py.test pykeyvi/tests/
