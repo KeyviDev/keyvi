54147f5f64459525640d3c217de235d95a0aa1c1
Narek Gharibyan <narekgharibyan@gmail.com>
2016-09-22 12:00:45 +0200

    renaming

--- travis/build_manylinux_package.sh
+++ travis/build_manylinux_package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 
-docker run --rm -v `pwd`:/keyvi $DOCKER_IMAGE /keyvi/travis/build_wheels.sh
+docker run --rm -v `pwd`:/keyvi $DOCKER_IMAGE /keyvi/travis/build_manylinux_wheels.sh
 
 pip install --user pykeyvi/wheelhouse/python_keyvi*.whl
 py.test pykeyvi/tests/
