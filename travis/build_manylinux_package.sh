9c42e35501557597b3696870b0d11313aadfe716
Narek Gharibyan <narekgharibyan@gmail.com>
2016-09-22 10:59:45 +0200

    fixed build_wheels path

--- travis/build_manylinux_package.sh
+++ travis/build_manylinux_package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 
-docker run --rm -v `pwd`:/keyvi $DOCKER_IMAGE /keyvi/build_wheels.sh
+docker run --rm -v `pwd`:/keyvi $DOCKER_IMAGE /keyvi/travis/build_wheels.sh
 
 pip install --user python_keyvi -f wheelhouse/
 py.test tests/
