eb1b938df96ca7622126e339809acf8da9f98868
Narek Gharibyan <narekgharibyan@gmail.com>
2016-10-10 12:04:14 +0200

    - disabled cpp unit tests for now (they are timing out) - added mac_os wheel build - using default boost

---
+++ travis/build_osx_package.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ev
+
+cd keyvi
+scons -j 4 mode=release
+cd ..
+
+cd pykeyvi
+python setup.py bdist_wheel -d wheelhouse
+pip install wheelhouse/*.whl
+py.test tests
+cd ..
