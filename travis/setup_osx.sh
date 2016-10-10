0cb119019a64b4ceb92c9373b1cb7ce1ce58a697
Narek Gharibyan <narekgharibyan@gmail.com>
2016-10-10 11:28:42 +0200

    installing boost using brew bottles

--- travis/setup_osx.sh
+++ travis/setup_osx.sh
@@ -5,5 +5,6 @@ brew update
 brew install snappy scons
 
 brew uninstall boost
-wget 'https://github.com/hendrik-cliqz/travis-homebrew-bottle/releases/download/boost-1.61-cxx11/boost-1.61.0_1.mavericks.bottle.1.tar.gz'
-brew install boost-1.61.0_1.mavericks.bottle.1.tar.gz
+#wget 'https://github.com/hendrik-cliqz/travis-homebrew-bottle/releases/download/boost-1.61-cxx11/boost-1.61.0_1.mavericks.bottle.1.tar.gz'
+#brew install boost-1.61.0_1.mavericks.bottle.1.tar.gz
+brew install --force-bottles boost
