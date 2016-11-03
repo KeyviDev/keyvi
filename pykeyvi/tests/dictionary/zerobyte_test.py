bbd2928202e08ce9e39e23220cbc3131f865f8fe
Hendrik Muhs <hendrik.muhs@gmail.com>
2016-11-03 20:12:46 +0100

    add zero-byte python test

---
+++ pykeyvi/tests/dictionary/zerobyte_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Usage: py.test tests
+
+import contextlib
+import os
+
+import pykeyvi
+
+import sys
+import os
+
+root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(root, "../"))
+
+from test_tools import tmp_dictionary
+
+def test_zerobyte():
+    c=pykeyvi.JsonDictionaryCompiler()
+    c.Add("\x00abc", '["a" : 2]')
+    c.Add("abc\x00def", '["a" : 3]')
+    c.Add("cd\x00", '["a" : 4]')
+    with tmp_dictionary(c, 'zerobyte.kv') as d:
+        assert d["\x00abc"].GetValue() == '["a" : 2]'
+        assert d["abc\x00def"].GetValue() == '["a" : 3]'
+        assert d["cd\x00"].GetValue() == '["a" : 4]'
+        assert len([(k, v) for k, v in d.GetAllItems()]) == 3
