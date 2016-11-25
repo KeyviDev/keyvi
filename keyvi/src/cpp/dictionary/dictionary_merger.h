cd9b573cd744f80bd46d2e4a8089f9fb290b45cc
Narek Gharibyan <narekgharibyan@gmail.com>
2016-11-25 10:56:35 +0100

    reformatting

--- keyvi/src/cpp/dictionary/dictionary_merger.h
+++ keyvi/src/cpp/dictionary/dictionary_merger.h
@@ -90,102 +90,105 @@ private:
         int priority_;
     };
 
- public:
-  DictionaryMerger(size_t memory_limit = 1073741824,
-                   const merger_param_t& params = merger_param_t()):
-                     dicts_to_merge_(), memory_limit_(memory_limit), params_(params){
-    if (params_.count(TEMPORARY_PATH_KEY) == 0) {
-      params_[TEMPORARY_PATH_KEY] = boost::filesystem::temp_directory_path().string();
-    }
-  }
-
-  void Add (const std::string& filename){
-    fsa::automata_t fsa (new fsa::Automata(filename.c_str()));
-
-    if (fsa->GetValueStore()->GetValueStoreType() != ValueStoreT::GetValueStoreType()) {
-      throw std::invalid_argument("Dictionaries must have the same type.");
-    }
-
-    dicts_to_merge_.push_back(fsa);
-  }
-
-  /**
-   * Set a custom manifest to be embedded into the index file.
-   *
-   * @param manifest as JSON string
-   */
-  void SetManifestFromString(const std::string& manifest){
-    manifest_ = manifest;
-  }
-
-  void Merge(const std::string& filename){
-    std::priority_queue<SegmentIterator> pqueue;
-
-    int i = 0;
-    for (auto fsa: dicts_to_merge_) {
-      fsa::EntryIterator e_it(fsa);
-      pqueue.push(SegmentIterator(e_it, i++));
-    }
-
-    ValueStoreT* value_store = new ValueStoreT(params_);
-
-    fsa::Generator<PersistenceT, ValueStoreT> generator(memory_limit_, params_, value_store);
-
-    std::string top_key;
-
-    while(!pqueue.empty()){
-      auto segment_it = pqueue.top();
-      pqueue.pop();
-
-      top_key = segment_it.entryIterator().GetKey();
-
-      // check for same keys and merge only the most recent one
-      while (!pqueue.empty() and pqueue.top().entryIterator().operator==(top_key)) {
-
-        auto to_inc = pqueue.top();
-        TRACE("removing element with prio %d (in favor of %d)", to_inc.priority, entry_it.priority);
-
-        pqueue.pop();
-        if (++to_inc) {
-          TRACE("push iterator");
-          pqueue.push(to_inc);
+public:
+    DictionaryMerger(size_t memory_limit = 1073741824, const merger_param_t& params = merger_param_t())
+            : dicts_to_merge_(),
+              memory_limit_(memory_limit),
+              params_(params)
+    {
+        if (params_.count(TEMPORARY_PATH_KEY) == 0) {
+            params_[TEMPORARY_PATH_KEY] = boost::filesystem::temp_directory_path().string();
         }
-      }
-
-      fsa::ValueHandle handle;
-      handle.no_minimization = false;
-
-      // Todo: if inner weights are used update them
-      //handle.weight = value_store_->GetWeightValue(value);
-      handle.weight = 0;
-
-      handle.value_idx = value_store->GetValue(segment_it.entryIterator().GetFsa()->GetValueStore()->GetValueStorePayload(),
-                                               segment_it.entryIterator().GetValueId(),
-                                               handle.no_minimization);
-
-      TRACE("Add key: %s", top_key.c_str());
-      generator.Add(std::move(top_key), handle);
-
-      if (++segment_it) {
-        pqueue.push(segment_it);
-      }
     }
 
-    dicts_to_merge_.clear();
+    void Add(const std::string& filename) {
+        fsa::automata_t fsa(new fsa::Automata(filename.c_str()));
 
-    TRACE("finished iterating, do final compile.");
+        if (fsa->GetValueStore()->GetValueStoreType() != ValueStoreT::GetValueStoreType()) {
+            throw std::invalid_argument("Dictionaries must have the same type.");
+        }
 
-    generator.CloseFeeding();
+        dicts_to_merge_.push_back(fsa);
+    }
 
-    generator.SetManifestFromString(manifest_);
-    generator.WriteToFile(filename);
-  }
+    /**
+     * Set a custom manifest to be embedded into the index file.
+     *
+     * @param manifest as JSON string
+     */
+    void SetManifestFromString(const std::string& manifest) {
+        manifest_ = manifest;
+    }
 
- private:
-  std::vector<fsa::automata_t> dicts_to_merge_;
-  size_t memory_limit_;
-  fsa::internal::IValueStoreWriter::vs_param_t params_;
-  std::string manifest_ = std::string();
+    void Merge(const std::string& filename) {
+        std::priority_queue<SegmentIterator> pqueue;
+
+        int i = 0;
+        for (auto fsa: dicts_to_merge_) {
+            fsa::EntryIterator e_it(fsa);
+            pqueue.push(SegmentIterator(e_it, i++));
+        }
+
+        ValueStoreT* value_store = new ValueStoreT(params_);
+
+        fsa::Generator<PersistenceT, ValueStoreT> generator(memory_limit_, params_, value_store);
+
+        std::string top_key;
+
+        while (!pqueue.empty()) {
+            auto segment_it = pqueue.top();
+            pqueue.pop();
+
+            top_key = segment_it.entryIterator().GetKey();
+
+            // check for same keys and merge only the most recent one
+            while (!pqueue.empty() and pqueue.top().entryIterator().operator==(top_key)) {
+
+                auto to_inc = pqueue.top();
+                TRACE("removing element with prio %d (in favor of %d)", to_inc.priority, entry_it.priority);
+
+                pqueue.pop();
+                if (++to_inc) {
+                    TRACE("push iterator");
+                    pqueue.push(to_inc);
+                }
+            }
+
+            fsa::ValueHandle handle;
+            handle.no_minimization = false;
+
+            // Todo: if inner weights are used update them
+            //handle.weight = value_store_->GetWeightValue(value);
+            handle.weight = 0;
+
+            handle.value_idx = value_store->GetValue(
+                    segment_it.entryIterator().GetFsa()->GetValueStore()->GetValueStorePayload(),
+                    segment_it.entryIterator().GetValueId(),
+                    handle.no_minimization);
+
+            TRACE("Add key: %s", top_key.c_str());
+            generator.Add(std::move(top_key), handle);
+
+            if (++segment_it) {
+                pqueue.push(segment_it);
+            }
+        }
+
+        dicts_to_merge_.clear();
+
+        TRACE("finished iterating, do final compile.");
+
+        generator.CloseFeeding();
+
+        generator.SetManifestFromString(manifest_);
+        generator.WriteToFile(filename);
+    }
+
+private:
+    std::vector<fsa::automata_t> dicts_to_merge_;
+    size_t memory_limit_;
+    fsa::internal::IValueStoreWriter::vs_param_t params_;
+    std::string manifest_ = std::string();
 };
 
 } /* namespace dictionary */
