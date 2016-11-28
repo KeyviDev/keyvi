86ee994f9a37d01c877f7f71f9abedde42a6c380
Narek Gharibyan <narekgharibyan@gmail.com>
2016-11-28 13:28:43 +0100

    - added license header

--- keyvi/src/cpp/dictionary/util/keyvi_file.h
+++ keyvi/src/cpp/dictionary/util/keyvi_file.h
@@ -1,6 +1,28 @@
-//
-// Created by Narek Gharibyan on 11/21/16.
-//
+/* * keyvi - A key value store.
+ *
+ * Copyright 2015, 2016 Hendrik Muhs<hendrik.muhs@gmail.com>,
+ *                      Narek Gharibyan<narekgharibyan@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/*
+ * keyvi_file.h
+ *
+ *  Created on: November 21, 2016
+ *      Author: Narek Gharibyan <narekgharibyan@gmail.com>
+ */
 
 #ifndef KEYVI_FILE_H
 #define KEYVI_FILE_H
