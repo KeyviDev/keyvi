f43e931b65c8a62d3f47aa083349bfdd29fc8bcf
Narek Gharibyan <narekgharibyan@gmail.com>
2016-11-28 11:43:22 +0100

    - added 'stats' implementation to keyvi cli

--- pykeyvi/keyvicli/cli.py
+++ pykeyvi/keyvicli/cli.py
@@ -1,5 +1,25 @@
+import json
+import pykeyvi
+
+from argparse import ArgumentParser
+
+
 def main():
-    pass
+    argument_parser = ArgumentParser(description='keyvi')
+    subparsers = argument_parser.add_subparsers(dest='command')
+
+    stats_parser = subparsers.add_parser('stats')
+    stats_parser.add_argument('input_file', type=str, metavar='FILE')
+
+    args = argument_parser.parse_args()
+
+    if args.command == 'stats':
+        print json.dumps(pykeyvi.Dictionary(args.input_file).GetStatistics(), indent=4, sort_keys=True)
+    # elif args.command == 'compile':
+    #     pass
+    else:
+        return 'Must never reach here'
+
 
 if __name__ == '__main__':
     main()
