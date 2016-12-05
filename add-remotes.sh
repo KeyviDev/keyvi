07928392ae75b6ffb31e68a45a1b7bd49d277b43
Narek Gharibyan <narekgharibyan@gmail.com>
2016-12-05 12:00:00 +0100

    - updated remotes urls

--- add-remotes.sh
+++ add-remotes.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
-git remote add keyvi git@github.com:cliqz/keyvi.git
-git remote add rapidjson git@github.com:miloyip/rapidjson.git
-git remote add msgpack-c git@github.com:msgpack/msgpack-c.git
-git remote add tpie git@github.com:thomasmoelhave/tpie.git
-git remote add mprpc git@github.com:hendrik-cliqz/mprpc.git
-git remote add xchange git@github.com:xpol/xchange.git
+#!/usr/bin/env bash
+set -ev
+
+git remote add keyvi        https://github.com/cliqz-oss/keyvi.git
+git remote add rapidjson    https://github.com/miloyip/rapidjson.git
+git remote add msgpack-c    https://github.com/msgpack/msgpack-c.git
+git remote add tpie         https://github.com/thomasmoelhave/tpie.git
+git remote add xchange      https://github.com/xpol/xchange.git
