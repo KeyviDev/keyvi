fc9f16551c8cad42b6b32a54722813f95bfd9785
Narek Gharibyan <narekgharibyan@gmail.com>
2016-12-05 01:28:07 +0100

    - added  'dump' sub-command  - moved 'stats' impl into the function

--- pykeyvi/keyvicli/cli.py
+++ pykeyvi/keyvicli/cli.py
@@ -4,6 +4,20 @@ import pykeyvi
 from argparse import ArgumentParser
 
 
+def stats(input_file):
+    print json.dumps(pykeyvi.Dictionary(input_file).GetStatistics(), indent=4, sort_keys=True)
+
+
+def dump(input_file, output_file):
+    dictionary = pykeyvi.Dictionary(input_file)
+    with open(output_file, 'w') as file_out:
+        for key, value in dictionary.GetAllItems():
+            file_out.write(key)
+            if value:
+                file_out.write('\t' + value)
+            file_out.write('\n')
+
+
 def main():
     argument_parser = ArgumentParser(description='keyvi')
     subparsers = argument_parser.add_subparsers(dest='command')
@@ -11,12 +25,16 @@ def main():
     stats_parser = subparsers.add_parser('stats')
     stats_parser.add_argument('input_file', type=str, metavar='FILE')
 
+    dump_parser = subparsers.add_parser('dump')
+    dump_parser.add_argument('input_file', type=str, metavar='FILE')
+    dump_parser.add_argument('output_file', type=str, metavar='OUT_FILE')
+
     args = argument_parser.parse_args()
 
     if args.command == 'stats':
-        print json.dumps(pykeyvi.Dictionary(args.input_file).GetStatistics(), indent=4, sort_keys=True)
-    # elif args.command == 'compile':
-    #     pass
+        stats(args.input_file)
+    elif args.command == 'dump':
+        dump(args.input_file, args.output_file)
     else:
         return 'Must never reach here'
 
