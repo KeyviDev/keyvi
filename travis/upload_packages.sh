6f8065f59b38d583503421b230de4f248e9e1bd9
Hendrik Muhs <hendrik.muhs@gmail.com>
2016-12-21 14:28:26 +0100

    fix package upload

--- travis/upload_packages.sh
+++ travis/upload_packages.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -ev
 
+# use python from pyenv
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
+
 cd pykeyvi
 if [ -n "$(ls -A wheelhouse)" ]; then
     twine upload --config-file ../travis/pypirc -u $PYPI_USERNAME -p $PYPI_PASSWORD wheelhouse/*
