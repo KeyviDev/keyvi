c477453fc10f567cfbe7ffe44c08ff87c54734e1
Hendrik Muhs <hendrik.muhs@gmail.com>
2016-12-21 11:31:56 +0100

    travis support for python3

--- travis/build_manylinux_wheels.sh
+++ travis/build_manylinux_wheels.sh
@@ -3,7 +3,22 @@ set -ev
 
 cd /keyvi/pykeyvi/
 
-PYBIN=/opt/python/cp27-cp27mu/bin
+case "${PYTHON_VERSION}" in
+  27)
+      PYBIN=/opt/python/cp27-cp27mu/bin
+      ;;
+  33)
+      PYBIN=/opt/python/cp33-cp33m/bin
+      ;;
+  34)
+      PYBIN=/opt/python/cp34-cp34m/bin
+      ;;
+  35)
+      PYBIN=/opt/python/cp35-cp35m/bin
+      ;;
+  *)
+      echo "PYTHON_VERSION not set"
+esac
 
 ${PYBIN}/python setup.py bdist_wheel
 
