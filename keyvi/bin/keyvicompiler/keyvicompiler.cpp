6c37403a18a84aefbadacdd1c6139378ccda7cf5
Hendrik Muhs <hendrik.muhs@gmail.com>
2017-12-10 10:36:59 +0100

    repair memory limit parameter

--- keyvi/bin/keyvicompiler/keyvicompiler.cpp
+++ keyvi/bin/keyvicompiler/keyvicompiler.cpp
@@ -250,7 +250,8 @@ int main(int argc, char** argv) {
 
   description.add_options()("input-file,i", boost::program_options::value<std::vector<std::string>>(), "input file");
   description.add_options()("output-file,o", boost::program_options::value<std::string>(), "output file");
-  description.add_options()("memory-limit,m", boost::program_options::value<size_t>(), "amount of main memory to use");
+  description.add_options()("memory-limit,m", boost::program_options::value<std::string>(),
+                            "amount of main memory to use");
   description.add_options()("dictionary-type,d", boost::program_options::value<std::string>()->default_value("integer"),
                             "type of dictionary (integer (default), string, key-only, json, completion)");
   description.add_options()("parameter,p", boost::program_options::value<std::vector<std::string>>()
