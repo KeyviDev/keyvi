d411e2fa6c9a13a67e42e65a0d747cc3f930d061
Hendrik Muhs <hendrik.muhs@gmail.com>
2017-12-27 11:18:50 +0100

    active object improvements and test

--- keyvi/include/keyvi/util/active_object.h
+++ keyvi/include/keyvi/util/active_object.h
@@ -25,6 +25,7 @@
 #ifndef KEYVI_UTIL_ACTIVE_OBJECT_H_
 #define KEYVI_UTIL_ACTIVE_OBJECT_H_
 
+#include <algorithm>
 #include <chrono>  // NOLINT
 #include <functional>
 #include <thread>  // NOLINT
@@ -41,8 +42,13 @@ template <typename T, size_t Tsize = 100>
 class ActiveObject final {
  public:
   explicit ActiveObject(T* resource, const std::function<void()>& scheduled_task,
-                        const std::chrono::duration<double>& flush_interval = std::chrono::milliseconds(1000))
-      : resource_(resource), scheduled_task_(scheduled_task), queue_(flush_interval), done_(false) {
+                        const std::chrono::milliseconds& flush_interval = std::chrono::milliseconds(1000))
+      : resource_(resource),
+        flush_interval_(flush_interval),
+        scheduled_task_(scheduled_task),
+        scheduled_task_next_run_(std::chrono::system_clock::now() + flush_interval_),
+        queue_(std::min(std::chrono::milliseconds(5), flush_interval_)),
+        done_(false) {
     worker_ = std::thread([this] {
       std::function<void()> item;
       while (!done_) {
@@ -50,16 +56,18 @@ class ActiveObject final {
           item();
         }
 
-        // todo: run only if flush interval has passed
-        scheduled_task_();
+        // run only if flush interval has passed
+        auto tp = std::chrono::system_clock::now();
+        if (tp > scheduled_task_next_run_) {
+          scheduled_task_next_run_ = tp + flush_interval_;
+          scheduled_task_();
+        }
       }
     });
   }
 
   ~ActiveObject() {
-    queue_.Push([this] {
-      done_ = true;
-    });
+    queue_.Push([this] { done_ = true; });
 
     worker_.join();
   }
@@ -74,7 +82,11 @@ class ActiveObject final {
 
   T* resource_;
 
+  std::chrono::milliseconds flush_interval_;
+
   std::function<void()> scheduled_task_;
+
+  std::chrono::time_point<std::chrono::system_clock> scheduled_task_next_run_;
 
   std::thread worker_;
 
