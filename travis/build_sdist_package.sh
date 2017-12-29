8d5bf70a1b319ce72736a56650f3bb92accbc0ae
Hendrik Muhs <hendrik.muhs@elastic.co>
2017-12-29 20:11:59 +0100

    debug travis problem
    try a workaround

--- travis/build_sdist_package.sh
+++ travis/build_sdist_package.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -ev
 
+# https://github.com/travis-ci/travis-ci/issues/8920
+python -c "import fcntl; fcntl.fcntl(1, fcntl.F_SETFL, 0)"
+
 cd python
 python setup.py sdist -d wheelhouse
 
-#pip uninstall -y autowrap
-#pip install wheelhouse/*.tar.gz
-#py.test tests
-#cd ..
+pip uninstall -y autowrap
+pip install wheelhouse/*.tar.gz
+py.test tests
+cd ..
