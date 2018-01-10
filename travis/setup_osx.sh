bea3dac3889a0a63cc0cb38ba94dd20f67d6562d
Hendrik Muhs <hendrik.muhs@gmail.com>
2018-01-10 13:29:40 +0100

    upgrade zlib

--- travis/setup_osx.sh
+++ travis/setup_osx.sh
@@ -5,6 +5,7 @@ diskutil erasevolume HFS+ 'ram-disk' `hdiutil attach -nomount ram://6165430`
 df -h
 
 brew update
+brew install zlib
 brew install snappy
 sudo -H easy_install pip
 sudo -H pip install wheel
