9b4d7be8272c5bbdac520502428a69d60d492223
Hendrik Muhs <hendrik.muhs@gmail.com>
2018-01-20 09:24:09 +0100

    improve segment key deletion test

--- keyvi/tests/keyvi/index/internal/segment_test.cpp
+++ keyvi/tests/keyvi/index/internal/segment_test.cpp
@@ -16,7 +16,10 @@
 // limitations under the License.
 //
 
+#include <algorithm>
 #include <boost/test/unit_test.hpp>
+
+#include <msgpack.hpp>
 
 #include "index/internal/segment.h"
 #include "testing/temp_dictionary.h"
@@ -27,18 +30,49 @@ namespace internal {
 
 BOOST_AUTO_TEST_SUITE(SegmentTests)
 
+void LoadDeletedKeys(const std::string& filename, std::vector<std::string>* deleted_keys) {
+  std::ifstream deleted_keys_stream(filename, std::ios::binary);
+
+  BOOST_CHECK(deleted_keys_stream.good());
+
+  std::stringstream buffer;
+  buffer << deleted_keys_stream.rdbuf();
+
+  msgpack::unpacked unpacked_object;
+  msgpack::unpack(unpacked_object, buffer.str().data(), buffer.str().size());
+
+  unpacked_object.get().convert(deleted_keys);
+  std::sort(deleted_keys->begin(), deleted_keys->end());
+}
+
 BOOST_AUTO_TEST_CASE(deletekey) {
-  std::vector<std::pair<std::string, std::string>> test_data = {{"abc", "{a:1}"}, {"abbc", "{b:2}"}};
+  std::vector<std::pair<std::string, std::string>> test_data = {
+      {"abc", "{a:1}"}, {"abbc", "{b:2}"}, {"cde", "{c:2}"}, {"fgh", "{g:6}"}, {"tyc", "{o:2}"}};
   testing::TempDictionary dictionary = testing::TempDictionary::makeTempDictionaryFromJson(&test_data);
 
   segment_t segment(new Segment(dictionary.GetFileName()));
 
   segment->DeleteKey("abc");
   segment->Persist();
+
+  std::vector<std::string> deleted_keys;
+  LoadDeletedKeys(dictionary.GetFileName() + ".dk", &deleted_keys);
+
+  BOOST_CHECK_EQUAL(1, deleted_keys.size());
+  BOOST_CHECK_EQUAL("abc", deleted_keys[0]);
+
+  segment->DeleteKey("tyc");
+  segment->Persist();
+
+  LoadDeletedKeys(dictionary.GetFileName() + ".dk", &deleted_keys);
+
+  BOOST_CHECK_EQUAL(2, deleted_keys.size());
+  BOOST_CHECK_EQUAL("abc", deleted_keys[0]);
+  BOOST_CHECK_EQUAL("tyc", deleted_keys[1]);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
 
-} /* namespace internal */
-} /* namespace index */
-} /* namespace keyvi */
+}  // namespace internal
+}  // namespace index
+}  // namespace keyvi
