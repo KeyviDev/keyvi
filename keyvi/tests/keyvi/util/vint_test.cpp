3f8cfcf4c5ed731f5beab34d6addd87dd8cbf725
Hendrik Muhs <hendrik.muhs@elastic.co>
2018-01-31 12:59:29 +0100

    fix a bug in variable length short calculation which can cause corrupt keyvi files (#32)
    fixes a off by 1 miscalculation between the length calculated by getVarshortLength and encodeVarshort, while the 1st returned 2 for 0x7fff the 2nd correctly returned 1 as 0x7fff fits in 1 byte (0111 1111 1111 1111 ). The same bug existed for the next boundary 0x3fffffff.
    
    As a result of this miscalculation it was possible - although rather unlikely - that a bit in a bitvector was set, marking a written cell while it did not got written as the required space was 1 instead of 2. The consequence of the unwritten cell is a default to '\0', this '\0' caused a mismatch starting with version 0.2 where '\0' became a valid transition. This broken transition caused a segfault due to a broken pointer.

--- keyvi/tests/keyvi/util/vint_test.cpp
+++ keyvi/tests/keyvi/util/vint_test.cpp
@@ -58,16 +58,33 @@ BOOST_AUTO_TEST_CASE(VShortLength) {
   BOOST_CHECK_EQUAL(77777, decodeVarshort(buffer));
 
   encodeVarshort(32767, buffer, &size);
-  BOOST_CHECK_EQUAL(util::getVarshortLength(1), size);
+  BOOST_CHECK_EQUAL(util::getVarshortLength(32767), size);
+  BOOST_CHECK_EQUAL(1, size);
   BOOST_CHECK_EQUAL(32767, decodeVarshort(buffer));
+
+  encodeVarshort(32768, buffer, &size);
+  BOOST_CHECK_EQUAL(util::getVarshortLength(32768), size);
+  BOOST_CHECK_EQUAL(2, size);
+  BOOST_CHECK_EQUAL(32768, decodeVarshort(buffer));
+
+  encodeVarshort(0x3fffffff, buffer, &size);
+  BOOST_CHECK_EQUAL(util::getVarshortLength(0x3fffffff), size);
+  BOOST_CHECK_EQUAL(2, size);
+  BOOST_CHECK_EQUAL(0x3fffffff, decodeVarshort(buffer));
 
   encodeVarshort(0x40000000, buffer, &size);
   BOOST_CHECK_EQUAL(util::getVarshortLength(0x40000000), size);
+  BOOST_CHECK_EQUAL(3, size);
   BOOST_CHECK_EQUAL(0x40000000, decodeVarshort(buffer));
 
-  encodeVarshort(0x200000000000, buffer, &size);
+  encodeVarshort(0x1fffffffffff, buffer, &size);
+  BOOST_CHECK_EQUAL(util::getVarshortLength(0x1fffffffffff), size);
+  BOOST_CHECK_EQUAL(3, size);
+  BOOST_CHECK_EQUAL(0x1fffffffffff, decodeVarshort<uint64_t>(buffer));
 
+  encodeVarshort(0x200000000000, buffer, &size);
   BOOST_CHECK_EQUAL(util::getVarshortLength(0x200000000000), size);
+  BOOST_CHECK_EQUAL(4, size);
   BOOST_CHECK_EQUAL(0x200000000000, decodeVarshort<uint64_t>(buffer));
 
   uint64_t x = 11687;
