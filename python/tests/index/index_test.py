e71040ef1e456b526da401fef4aba06144833b6c
Hendrik Muhs <hendrik.muhs@gmail.com>
2018-01-31 18:57:10 +0100

    add a python test with some indexing

--- python/tests/index/index_test.py
+++ python/tests/index/index_test.py
@@ -8,12 +8,37 @@ import tempfile
 
 
 def test_open_index():
-    test_dir = os.path.join(tempfile.gettempdir(), "index_writer_test", "index")
+    test_dir = os.path.join(tempfile.gettempdir(), "index_open_index")
     try:
-        os.makedirs(test_dir)
+        os.makedirs(test_dir, exist_ok=True)
         index = Index(os.path.join(test_dir, "index"))
         index.Set("a", "{}")
         del index
+        index = Index(os.path.join(test_dir, "index"))
+        assert "a" in index
     finally:
         shutil.rmtree(test_dir)
     
+def test_some_indexing():
+    test_dir = os.path.join(tempfile.gettempdir(), "index_some_indexing")
+    iterations = 10000
+    split = 2000
+    try:
+        os.makedirs(test_dir, exist_ok=True)
+        index = Index(os.path.join(test_dir, "index"))
+        for i in range (0, iterations):
+            index.Set("key-{}".format(i), "value-{}".format(i))
+        index.Flush()
+        for i in range (split, iterations):
+            assert "key-{}".format(i) in index
+            index.Delete("key-{}".format(i))
+        index.Flush()
+
+        for i in range (0, split):
+            assert "key-{}".format(i) in index
+
+        for i in range (split, iterations):
+            assert not "key-{}".format(i) in index
+
+    finally:
+        shutil.rmtree(test_dir)
