f7ba75c5146885e8f1259f023dbf58f41b7b2e8a
Hendrik Muhs <hendrik.muhs@gmail.com>
2018-02-20 21:15:43 +0100

    add a keyvimerger python version

--- python/src/addons/autwrap_workarounds.pyx
+++ python/src/addons/autwrap_workarounds.pyx
@@ -7,11 +7,22 @@ import json
 import msgpack
 import keyvi._pycore
 import os.path
+import sys
 
-def get_bin_folder():
+
+def get_package_root():
     module_location = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(keyvi._pycore.__file__)), ".."))
 
     if (PY_MAJOR_VERSION >= 3):
         module_location = module_location.encode('utf-8')
 
-    return os.path.join(module_location, b"_bin")
+    return module_location
+
+
+def get_interpreter_executable():
+    executable = sys.executable
+
+    if (PY_MAJOR_VERSION >= 3):
+        executable = executable.encode('utf-8')
+
+    return executable
--- python/tests/index/merger_binary_test.py
+++ python/tests/index/merger_binary_test.py
@@ -8,16 +8,25 @@ import subprocess
 import sys
 
 
-def get_bin_folder():
+def get_package_root():
     module_location = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(keyvi._pycore.__file__)), ".."))
 
     if (sys.version_info.major >= 3):
         module_location = module_location.encode('utf-8')
 
-    return os.path.join(module_location, b"_bin")
+    return module_location
+
+
+def get_interpreter_executable():
+    executable = sys.executable
+
+    if (sys.version_info.major >= 3):
+        executable = executable.encode('utf-8')
+
+    return executable
 
 
 def test_merger_binary():
-    cmd = os.path.join(get_bin_folder(), b"keyvimerger -h")
+    cmd = get_interpreter_executable() + b" " + os.path.join(get_package_root(), b"_pycore" , b"keyvimerger.py") + b" -h"
     rc = subprocess.call(cmd, shell=True)
     assert rc == 0
