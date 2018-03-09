d47008a763a9797b36c3615cc70673d35c91dd53
Hendrik Muhs <hendrik.muhs@elastic.co>
2018-03-09 21:59:00 +0100

    fix a couple of type issues in minimization hash (#51)
    fix a couple of type issues in minimization hash

--- keyvi/tests/keyvi/dictionary/fsa/internal/minimization_hash_test.cpp
+++ keyvi/tests/keyvi/dictionary/fsa/internal/minimization_hash_test.cpp
@@ -69,6 +69,16 @@ BOOST_AUTO_TEST_CASE(reset) {
   delete hash;
 }
 
+BOOST_AUTO_TEST_CASE(memory_usage) {
+  // default
+  MinimizationHash<PackedState<>> hash;
+  BOOST_CHECK_EQUAL(hash.GetMemoryUsage(), (8171 + 2042) * sizeof(PackedState<>));
+
+  // some non-default
+  MinimizationHash<PackedState<>> hash2(5);
+  BOOST_CHECK_EQUAL(hash2.GetMemoryUsage(), (32749 + 8187) * sizeof(PackedState<>));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 } /* namespace internal */
