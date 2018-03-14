2b16735c56c2ff1e0e3ae4a603e1c45dd8f2e029
Hendrik Muhs <hendrik.muhs@gmail.com>
2018-03-14 21:53:43 +0100

    simplify test for merger script

--- python/tests/index/merger_binary_test.py
+++ python/tests/index/merger_binary_test.py
@@ -1,29 +1,11 @@
 # -*- coding: utf-8 -*-
 # Usage: py.test tests
 
-import keyvi._pycore
+from keyvi._core import get_package_root, get_interpreter_executable
 
 import os.path
 import subprocess
 import sys
-
-
-def get_package_root():
-    module_location = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(keyvi._pycore.__file__)), ".."))
-
-    if (sys.version_info.major >= 3):
-        module_location = module_location.encode('utf-8')
-
-    return module_location
-
-
-def get_interpreter_executable():
-    executable = sys.executable
-
-    if (sys.version_info.major >= 3):
-        executable = executable.encode('utf-8')
-
-    return executable
 
 
 def test_merger_binary():
