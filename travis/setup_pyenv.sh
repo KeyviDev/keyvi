fcb91afba9414e3d576d7dd9bb0ab0ad1927df16
narekgharibyan <narekgharibyan@gmail.com>
2018-07-13 12:43:58 +0200

    - trying to workaround python 3.7.0 install issue

--- travis/setup_pyenv.sh
+++ travis/setup_pyenv.sh
@@ -7,11 +7,11 @@ if [ ! -d ~/.pyenv ]; then
   git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 fi
 
-pyenv install --list
-
 PYENV_ROOT="$HOME/.pyenv"
 PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
+pyenv install --list
 
 # Make sure the cache directory exists
 PYTHON_BUILD_CACHE_PATH="${PYTHON_BUILD_CACHE_PATH:-$HOME/.pyenv_cache}"
