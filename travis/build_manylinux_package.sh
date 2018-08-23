10b08bbe9a31452fb953e74ff9791eb07d07ff6f
narekgharibyan <narekgharibyan@gmail.com>
2018-08-23 17:07:53 +0200

    - using appropriate python interpreter

--- travis/build_manylinux_package.sh
+++ travis/build_manylinux_package.sh
@@ -3,6 +3,10 @@ set -ev
 
 docker run -e "PYTHON_VERSION=$PYTHON_VERSION" --rm -v `pwd`:/keyvi $DOCKER_IMAGE /keyvi/travis/build_manylinux_wheels.sh
 
+
+pyenv global $PYTHON_VERSION
+pip install -r python/requirements.txt
+
 pip install python/wheelhouse/keyvi*.whl
 py.test python/tests/
 py.test python/integration-tests/
