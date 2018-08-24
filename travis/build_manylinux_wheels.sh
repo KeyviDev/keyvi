a79aaf4bff66afb75cb755210e1605637067b3ab
narekgharibyan <narekgharibyan@gmail.com>
2018-08-24 10:29:05 +0200

    - fixing manylinux builds

--- travis/build_manylinux_wheels.sh
+++ travis/build_manylinux_wheels.sh
@@ -1,42 +1,64 @@
 #!/usr/bin/env bash
-set -ev
+#set -ev
+#
+#cd /keyvi/python/
+#
+##case "${PYTHON_VERSION}" in
+##  27)
+##      PYBIN=/opt/python/cp27-cp27mu/bin
+##      ;;
+##  33)
+##      PYBIN=/opt/python/cp33-cp33m/bin
+##      ;;
+##  34)
+##      PYBIN=/opt/python/cp34-cp34m/bin
+##      ;;
+##  35)
+##      PYBIN=/opt/python/cp35-cp35m/bin
+##      ;;
+##  36)
+##      PYBIN=/opt/python/cp36-cp36m/bin
+##      ;;
+##  37)
+##      PYBIN=/opt/python/cp37-cp37m/bin
+##      ;;
+##  pypy2)
+##      echo "pypy2 is not support at the moment, see: https://github.com/pypa/manylinux/issues/38"
+##      ;;
+##  *)
+##      echo "PYTHON_VERSION not set"
+##esac
+#
+#PYBIN=/opt/python/${PYTHON_PATH}/bin
+#
+#${PYBIN}/pip install -r requirements.txt
+#
+#${PYBIN}/python setup.py bdist_wheel
+#
+## Bundle external shared libraries into the wheels
+#for whl in dist/*.whl; do
+#    auditwheel show $whl
+#    auditwheel repair $whl -w wheelhouse/
+#done
+#
 
-cd /keyvi/python/
 
-#case "${PYTHON_VERSION}" in
-#  27)
-#      PYBIN=/opt/python/cp27-cp27mu/bin
-#      ;;
-#  33)
-#      PYBIN=/opt/python/cp33-cp33m/bin
-#      ;;
-#  34)
-#      PYBIN=/opt/python/cp34-cp34m/bin
-#      ;;
-#  35)
-#      PYBIN=/opt/python/cp35-cp35m/bin
-#      ;;
-#  36)
-#      PYBIN=/opt/python/cp36-cp36m/bin
-#      ;;
-#  37)
-#      PYBIN=/opt/python/cp37-cp37m/bin
-#      ;;
-#  pypy2)
-#      echo "pypy2 is not support at the moment, see: https://github.com/pypa/manylinux/issues/38"
-#      ;;
-#  *)
-#      echo "PYTHON_VERSION not set"
-#esac
+set -ex
 
 PYBIN=/opt/python/${PYTHON_PATH}/bin
 
-${PYBIN}/pip install -r requirements.txt
+"${PYBIN}/pip" install -r /io/python/requirements.txt
 
-${PYBIN}/python setup.py bdist_wheel
+"${PYBIN}/pip" wheel /io/python -w wheelhouse/ --no-deps
+
 
 # Bundle external shared libraries into the wheels
-for whl in dist/*.whl; do
-    auditwheel show $whl
-    auditwheel repair $whl -w wheelhouse/
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
 done
+
+
+"${PYBIN}/pip" install keyvi --no-index -f /io/wheelhouse
+
+py.test /io/python/tests/
+py.test /io/python/integration-tests/
