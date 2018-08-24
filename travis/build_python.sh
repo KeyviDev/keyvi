095494ad44ee173bf457e3a5e13dd4e4d8e3316b
narekgharibyan <narekgharibyan@gmail.com>
2018-08-24 15:33:57 +0200

    - minor

--- travis/build_python.sh
+++ travis/build_python.sh
@@ -3,13 +3,13 @@ set -ex
 
 cd /io
 
-pyenv global $PYTHON_VERSION
+pyenv global ${PYTHON_VERSION}
 
 cd python
 
 pip install -r requirements.txt
 
-python setup.py build --mode $CONF
+python setup.py build --mode ${CONF}
 python setup.py install --user
 py.test tests
 py.test integration-tests
