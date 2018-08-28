ea4f3b8a7e43984ccc0b09e0c98e34284847fa9f
narekgharibyan <narekgharibyan@gmail.com>
2018-08-28 17:09:07 +0200

    - building docs in docker image

--- travis/build_doc.sh
+++ travis/build_doc.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-set -ev
+set -ex
 
-./travis/generateDocumentationAndDeploy.sh
+docker run --rm -v `pwd`:/io \
+    -e TRAVIS_BUILD_NUMBER \
+    -e TRAVIS_COMMIT \
+    -e DOXYFILE='/io/keyvi/keyvi.Doxyfile' \
+    -e GH_REPO_NAME='keyvi' \
+    -e GH_REPO_REF='github.com/KeyviDev/keyvi.git' \
+    -e GH_REPO_TOKEN \
+    ${DOCKER_IMAGE} /io/travis/generateDocumentationAndDeploy.sh
