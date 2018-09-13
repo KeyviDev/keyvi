b3f6cc252a33569445ae06b75f0f106099a3da8d
Amit Verma <amit@cliqz.com>
2018-09-13 10:37:33 +0200

    Specify the memory limit in unit test.

--- keyvi/tests/keyvi/dictionary/dictionary_compiler_test.cpp
+++ keyvi/tests/keyvi/dictionary/dictionary_compiler_test.cpp
@@ -346,11 +346,9 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(DeleteUnsupported, SorterT, sorter_types) {
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(MultipleKeyUpdateAndCompile, SorterT, sorter_types) {
-  keyvi::util::parameters_t params = {{"a", "1"}};
-
   keyvi::dictionary::DictionaryCompiler<fsa::internal::SparseArrayPersistence<uint16_t>, fsa::internal::JsonValueStore,
                                         SorterT>
-      compiler(params);
+      compiler(keyvi::util::parameters_t({{"memory_limit_mb", "10"}}));
 
   compiler.Add("a", "1");
   compiler.Add("a", "1");
