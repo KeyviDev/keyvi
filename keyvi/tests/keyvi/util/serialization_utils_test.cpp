a1276f10f07bcee2d07292f22fb5fab17c98807c
Hendrik Muhs <hendrik.muhs@gmail.com>
2018-12-12 13:43:32 +0100

    add tests for serialization utils

--- keyvi/tests/keyvi/util/serialization_utils_test.cpp
+++ keyvi/tests/keyvi/util/serialization_utils_test.cpp
@@ -23,6 +23,8 @@
  *      Author: hendrik
  */
 
+#include <string>
+
 #include <boost/test/unit_test.hpp>
 
 #include "rapidjson/document.h"
@@ -37,10 +39,40 @@ BOOST_AUTO_TEST_SUITE(SerializationUtilsTests)
 
 BOOST_AUTO_TEST_CASE(GetUint64FromValueOrStringTest) {
   rapidjson::StringBuffer string_buffer;
-  rapidjson::Writer<rapidjson::StringBuffer> writer(string_buffer);
+
+  // create a test document
+  {
+    rapidjson::Writer<rapidjson::StringBuffer> writer(string_buffer);
+
+    writer.StartObject();
+    writer.Key("size");
+    writer.Uint64(42);
+    writer.Key("size_as_string");
+    writer.String(std::to_string(42));
+    writer.Key("uint64");
+    writer.Uint64(999);
+    writer.Key("uint64_as_string");
+    writer.String(std::to_string(999));
+    writer.Key("string");
+    writer.String("some_non_numeric_string");
+    writer.EndObject();
+  }
 
   rapidjson::Document d;
   d.Parse(string_buffer.GetString(), string_buffer.GetLength());
+  BOOST_CHECK_EQUAL(999ul, SerializationUtils::GetOptionalUInt64FromValueOrString(d, "uint64", 77ul));
+  BOOST_CHECK_EQUAL(999ul, SerializationUtils::GetOptionalUInt64FromValueOrString(d, "uint64_as_string", 77ul));
+  BOOST_CHECK_EQUAL(77ul, SerializationUtils::GetOptionalUInt64FromValueOrString(d, "uint64_not_there", 77ul));
+  BOOST_CHECK_THROW(SerializationUtils::GetOptionalUInt64FromValueOrString(d, "string", 77ul), boost::bad_lexical_cast);
+
+  BOOST_CHECK_EQUAL(42ul, SerializationUtils::GetOptionalSizeFromValueOrString(d, "size", 21ul));
+  BOOST_CHECK_EQUAL(42ul, SerializationUtils::GetOptionalSizeFromValueOrString(d, "size_as_string", 21ul));
+  BOOST_CHECK_EQUAL(21ul, SerializationUtils::GetOptionalSizeFromValueOrString(d, "size_not_there", 21ul));
+  BOOST_CHECK_THROW(SerializationUtils::GetOptionalSizeFromValueOrString(d, "string", 77ul), boost::bad_lexical_cast);
+
+  BOOST_CHECK_EQUAL(999ul, SerializationUtils::GetUint64FromValueOrString(d, "uint64"));
+  BOOST_CHECK_EQUAL(999ul, SerializationUtils::GetUint64FromValueOrString(d, "uint64_as_string"));
+  BOOST_CHECK_THROW(SerializationUtils::GetUint64FromValueOrString(d, "uint64_not_there"), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
