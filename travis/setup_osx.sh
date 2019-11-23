9cb8b236bf9e38565b81661b1817bab3b3e633ee
Hendrik Muhs <hendrik.muhs@gmail.com>
2019-11-23 20:28:48 +0100

    install openssl ourselves to fix osx problem for python 3.4.10

--- travis/setup_osx.sh
+++ travis/setup_osx.sh
@@ -7,11 +7,14 @@ df -h
 brew update
 brew install zlib
 brew install snappy
+brew install openssl@1.1
 
 brew upgrade pyenv
 
 export PATH="${HOME}/.pyenv/shims/:/root/.pyenv/bin:${PATH}"
 
+CFLAGS="-I$(brew --prefix openssl)/include" \
+LDFLAGS="-L$(brew --prefix openssl)/lib" \
 pyenv install ${PYTHON_VERSION}
 pyenv global ${PYTHON_VERSION}
 
