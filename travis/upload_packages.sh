d9db2a35678959a21738cbe303f9fa26bc502c98
Hendrik Muhs <hendrik.muhs@gmail.com>
2020-04-17 13:06:40 +0200

    remove set -e for package upload (#148)

--- travis/upload_packages.sh
+++ travis/upload_packages.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -ex
+set -x
 
 pip install --user wheel twine --upgrade
 
 cd python
 if [ -n "$(ls -A wheelhouse)" ]; then
-    twine upload --config-file ../travis/pypirc -u $PYPI_USERNAME -p $PYPI_PASSWORD wheelhouse/*
+    twine upload --config-file ../travis/pypirc -u $PYPI_USERNAME -p $PYPI_PASSWORD wheelhouse/* || travis_terminate 1;
 fi
