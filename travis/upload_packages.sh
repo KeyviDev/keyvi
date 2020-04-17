34a6ce7f92c6e8ac2ec289879ae56014db2b1b88
Hendrik Muhs <hendrik.muhs@gmail.com>
2020-04-17 21:56:19 +0200

    switch to python -m

--- travis/upload_packages.sh
+++ travis/upload_packages.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -x
 
-pip install --user wheel twine --upgrade
+python -m pip install --user wheel twine --upgrade
 
 cd python
 if [ -n "$(ls -A wheelhouse)" ]; then
-    twine upload --config-file ../travis/pypirc -u $PYPI_USERNAME -p $PYPI_PASSWORD wheelhouse/* || travis_terminate 1;
+    python -m twine upload --config-file ../travis/pypirc -u $PYPI_USERNAME -p $PYPI_PASSWORD wheelhouse/* || travis_terminate 1;
 fi
