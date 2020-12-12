58c62ffd7541884ffb328dc5f5071a9474e765af
Hendrik Muhs <hendrik.muhs@gmail.com>
2020-12-12 12:05:21 +0100

    Bump version and repair pypi upload (#201)
    bump version, repair package upload (backport from release-0.5.0 branch) and remove travis subfolder

--- travis/upload_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-python -m pip install --user wheel twine --upgrade
-
-cd python
-if [ -n "$(ls -A wheelhouse)" ]; then
-    python -m twine upload --config-file ../travis/pypirc -u $PYPI_USERNAME -p $PYPI_PASSWORD wheelhouse/* || travis_terminate 1;
-fi
