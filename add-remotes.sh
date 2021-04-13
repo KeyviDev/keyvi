662dbef938c6794b9c18f578e1c3cc7757cca111
Hendrik Muhs <hendrik.muhs@gmail.com>
2021-04-13 19:40:26 +0200

    update tiny process library origin

--- add-remotes.sh
+++ add-remotes.sh
@@ -4,5 +4,5 @@ set -ev
 git remote add keyvi        https://github.com/KeyviDev/keyvi.git
 git remote add rapidjson    https://github.com/miloyip/rapidjson.git
 git remote add msgpack-c    https://github.com/msgpack/msgpack-c.git
-git remote add tiny-process-library https://github.com/eidheim/tiny-process-library.git
+git remote add tiny-process-library https://gitlab.com/eidheim/tiny-process-library
 git remote add utfcpp       https://github.com/nemtrif/utfcpp.git
