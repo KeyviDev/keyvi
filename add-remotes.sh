7d11f2658c989cdbdff6b727adc1dd315bbd5b61
Hendrik Muhs <hendrik.muhs@gmail.com>
2021-04-20 12:55:39 +0200

    update remote for rapidjson

--- add-remotes.sh
+++ add-remotes.sh
@@ -2,7 +2,7 @@
 set -ev
 
 git remote add keyvi        https://github.com/KeyviDev/keyvi.git
-git remote add rapidjson    https://github.com/miloyip/rapidjson.git
+git remote add rapidjson    https://github.com/Tencent/rapidjson.git
 git remote add msgpack-c    https://github.com/msgpack/msgpack-c.git
 git remote add tiny-process-library https://gitlab.com/eidheim/tiny-process-library
 git remote add utfcpp       https://github.com/nemtrif/utfcpp.git
