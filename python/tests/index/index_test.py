b71cdd0141b03cccf1d4ef09f92879c69b651b1a
Hendrik Muhs <hendrik.muhs@gmail.com>
2021-05-23 11:09:56 +0200

    add a python test for GetNear

--- python/tests/index/index_test.py
+++ python/tests/index/index_test.py
@@ -113,3 +113,49 @@ def test_get_fuzzy():
         del index
     finally:
         shutil.rmtree(test_dir, ignore_errors=True)
+
+def test_get_near():
+    test_dir = os.path.join(tempfile.gettempdir(), "index_test_near")
+    try:
+        if not os.path.exists(test_dir):
+            os.mkdir(test_dir)
+        index = Index(os.path.join(test_dir, "index"))
+        # the following geohashes are created from openstreetmap coordinates and translated using a geohash encoder
+        index.Set("u21xj502gs79", "{'city' : 'Kobarid', 'country': 'si'}")
+        index.Set("u21xk2uxkhh2", "{'city' : 'Trnovo ob soci', 'country': 'si'}")
+        index.Set("u21x75n34qrp", "{'city' : 'Srpnecia', 'country': 'si'}")
+        index.Set("u21x6v1nx0c3", "{'city' : 'Zaga', 'country': 'si'}")
+        index.Set("u21xs20w9ssu", "{'city' : 'Cezsoca', 'country': 'si'}")
+        index.Set("u21x6yx5cqy6", "{'city' : 'Log Cezsoski', 'country': 'si'}")
+        index.Set("u21xs7ses4s3", "{'city' : 'Bovec', 'country': 'si'}")
+        index.Flush()
+
+        # some coordinate nearby, greedy false, so it prefers as close as possible 
+        matches = list(index.GetNear("u21xjjhhymt7", 4))
+        assert len(matches) == 1
+        assert  u'u21xj502gs79' == matches[0].GetMatchedString()
+        assert  u"{'city' : 'Kobarid', 'country': 'si'}" == matches[0].GetValue()
+
+        # greedy match, still closest should be the 1st match
+        matches = list(index.GetNear("u21xjjhhymt7", 4, True))
+        assert len(matches) == 7
+        assert  u'u21xj502gs79' == matches[0].GetMatchedString()
+        assert  u"{'city' : 'Kobarid', 'country': 'si'}" == matches[0].GetValue()
+
+        # closer match near Bovec and Cezsoca but closer to Cezsoca
+        matches = list(index.GetNear("u21xs20w9ssu", 5))
+        assert len(matches) == 1
+        assert  u'u21xs20w9ssu' == matches[0].GetMatchedString()
+        assert  u"{'city' : 'Cezsoca', 'country': 'si'}" == matches[0].GetValue()
+
+        # greedy should return Bovec, but not the other locations due to the prefix
+        matches = list(index.GetNear("u21xs20w9ssu", 5, True))
+        assert len(matches) == 2
+        assert  u'u21xs20w9ssu' == matches[0].GetMatchedString()
+        assert  u"{'city' : 'Cezsoca', 'country': 'si'}" == matches[0].GetValue()
+        assert  u'u21xs7ses4s3' == matches[1].GetMatchedString()
+        assert  u"{'city' : 'Bovec', 'country': 'si'}" == matches[1].GetValue()
+
+        del index
+    finally:
+        shutil.rmtree(test_dir, ignore_errors=True)
