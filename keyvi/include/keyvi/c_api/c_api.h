ecb95b8bf7a40edb41405967a293e8ec31d96d03
George Mossessian <george@constructor.io>
2021-05-27 14:56:06 -0500

    clang fmt

--- keyvi/include/keyvi/c_api/c_api.h
+++ keyvi/include/keyvi/c_api/c_api.h
@@ -75,7 +75,7 @@ struct keyvi_match_iterator* keyvi_dictionary_get_prefix_completions(const struc
 struct keyvi_match_iterator* keyvi_dictionary_get_all_prefix_completions(const struct keyvi_dictionary*, const char*,
                                                                          const size_t);
 
-struct keyvi_match_iterator* keyvi_dictionary_get_fuzzy(const struct keyvi_dictionary*, const char*, const size_t, 
+struct keyvi_match_iterator* keyvi_dictionary_get_fuzzy(const struct keyvi_dictionary*, const char*, const size_t,
                                                         const size_t);
 
 struct keyvi_match_iterator* keyvi_dictionary_get_multi_word_completions(const struct keyvi_dictionary*, const char*,
