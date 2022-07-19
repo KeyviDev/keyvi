418a3ca5981de42d76cba0b14546a4f25105f82d
Sergey Ivanychev <sergeyivanychev@gmail.com>
2022-07-19 10:46:22 +0400

    Build x86 + arm builder images

---
+++ docker/manylinux-builder/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+docker build . \
+    -f Dockerfile \
+    --build-arg base_image="quay.io/pypa/manylinux2014_x86_64" \
+    -t keyvidev/manylinux-builder && \
+    -t keyvidev/manylinux-builder-x86_64
+
+docker build . \
+    -f Dockerfile \
+    --build-arg base_image="quay.io/pypa/manylinux2014_aarch64" \
+    -t keyvidev/manylinux-builder-aarch64
+
+docker push keyvidev/manylinux-builder
+docker push keyvidev/manylinux-builder-x86_64
+docker push keyvidev/manylinux-builder-aarch64
