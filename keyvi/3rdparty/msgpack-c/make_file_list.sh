31dd8e04d3ab354369b0111ea884eb63668d2602
Hendrik Muhs <hendrik.muhs@gmail.com>
2024-01-06 19:59:31 +0100

    remove msgpack-c

--- keyvi/3rdparty/msgpack-c/make_file_list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find include -name "*.hpp" | sed -e 's/\s\+/\n/g' | LC_ALL=C sort > cpp_headers.tmp
-
-echo 'SET (msgpackc_HEADERS' > Files.cmake
-cat cpp_headers.tmp | sed -e 's/^/    /g' >> Files.cmake
-echo ')' >> Files.cmake
-
-rm -f cpp_headers.tmp
