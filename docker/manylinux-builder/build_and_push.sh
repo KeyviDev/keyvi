a9aabeb1a51bd62eb6f2b38af4b828932f201d1b
Hendrik Muhs <hendrik.muhs@gmail.com>
2024-01-08 12:06:03 +0100

    update docker files, more use of parallel building, add ccache and support of musllinux

--- docker/manylinux-builder/build_and_push.sh
+++ docker/manylinux-builder/build_and_push.sh
@@ -12,5 +12,17 @@ docker build . \
     --build-arg base_image="quay.io/pypa/manylinux2014_aarch64" \
     -t keyvidev/manylinux-builder-aarch64
 
+docker build . \
+    -f Dockerfile \
+    --build-arg base_image="quay.io/pypa/musllinux_1_1_x86_64" \
+    -t keyvidev/musllinux-builder-x86_64
+
+docker build . \
+    -f Dockerfile \
+    --build-arg base_image="quay.io/pypa/musllinux_1_1_aarch64" \
+    -t keyvidev/musllinux-builder-aarch64
+
 docker push keyvidev/manylinux-builder-x86_64
 docker push keyvidev/manylinux-builder-aarch64
+docker push keyvidev/musllinux-builder-x86_64
+docker push keyvidev/musllinux-builder-aarch64
