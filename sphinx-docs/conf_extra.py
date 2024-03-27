afff9a719380ef2d1086272a076d8c235686b2ff
Hendrik Muhs <hendrik.muhs@gmail.com>
2024-03-27 07:58:10 +0100

    switch to wagtail theme to provide more space for readability

--- sphinx-docs/conf_extra.py
+++ sphinx-docs/conf_extra.py
@@ -1,15 +1,23 @@
-from os.path import join, dirname, abspath
-import alabaster
+from os.path import join, dirname
+import sphinx_wagtail_theme
 
-html_theme = "alabaster"
+html_theme = "sphinx_wagtail_theme"
+html_theme_path = [sphinx_wagtail_theme.get_html_theme_path()]
 
 html_static_path = [join(dirname(__file__), "_static")]
 
-html_theme_options = {
-    "logo": "logo.png",
-    "logo_name": True,
-    "logo_text_align": "center",
-    "github_user": "keyvidev",
-    "github_repo": "keyvi",
-    "github_type": "star"
-}
+html_theme_options = dict(
+    project_name= "keyvi",
+    logo= "logo-white.png",
+    logo_alt = "keyvi",
+    github_url = "https://github.com/KeyviDev/keyvi/tree/master/sphinx-docs/",
+    footer_links = ",".join([
+        "Github|https://github.com/KeyviDev/keyvi",
+        "Pypi|https://pypi.org/project/keyvi/",
+    ]),
+ )
+
+html_show_copyright = False
+html_last_updated_fmt = "%b %d, %Y"
+
+html_css_files = ["custom.css"]
