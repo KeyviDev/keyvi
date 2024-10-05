2221036710adc6f9b77ad1595b4ad0f45fdf89e6
Hendrik Muhs <hendrik.muhs@gmail.com>
2024-10-05 15:13:00 +0200

    add pybind11 bindings
    add CI for pybind wrapper
    
    use macos-14 runner
    
    update tag
    
    disable macos-14
    
    remove integration tests
    
    skip pp37
    
    adapt sdist
    
    fix style
    
    fix style
    
    revert cibuildwheel changes
    
    try to fix sdist
    
    move keyvi source out of src
    
    install pytest
    
    add more match bindings
    
    move module import
    
    switch to scikit_build
    
    remove license file

---
+++ python-pybind/src/dictionary/py_match.cpp
@@ -0,0 +1,54 @@
+/* * keyvi - A key value store.
+ *
+ * Copyright 2024 Hendrik Muhs<hendrik.muhs@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <pybind11/pybind11.h>
+
+#include <memory>
+
+#include "keyvi/dictionary/dictionary.h"
+#include "keyvi/dictionary/match.h"
+
+#include "py_match_iterator.h"
+
+namespace py = pybind11;
+namespace kd = keyvi::dictionary;
+
+void init_keyvi_match(const py::module_ &m) {
+  py::module_ msgpack_ = py::module_::import("msgpack");
+
+  py::class_<kd::Match, std::shared_ptr<kd::Match>>(m, "Match")
+      .def(py::init<>())
+      .def_property("start", &kd::Match::GetStart, &kd::Match::SetStart)
+      .def_property("end", &kd::Match::GetEnd, &kd::Match::SetEnd)
+      .def_property("score", &kd::Match::GetScore, &kd::Match::SetScore)
+      .def_property("matched_string", &kd::Match::GetMatchedString, &kd::Match::SetMatchedString)
+      .def_property_readonly("value",
+                             [&msgpack_](const kd::Match &m) -> py::object {
+                               auto packed_value = m.GetMsgPackedValueAsString();
+                               if (packed_value.empty()) {
+                                 return py::none();
+                               }
+                               return msgpack_.attr("loads")(packed_value);
+                             })
+      .def("value_as_string", &kd::Match::GetValueAsString)
+      .def("raw_value_as_string", &kd::Match::GetRawValueAsString)
+      .def("__get_item__", &kd::Match::GetAttributePy)
+      // __setitem__
+      // dumps loads
+      .def_property_readonly("weight", &kd::Match::GetWeight)
+      .def("__bool__", [](const kd::Match &m) -> bool { return !m.IsEmpty(); });
+}
---
+++ python-pybind/tests/match_object_test.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+# Usage: py.test tests
+
+import keyvi_pybind11 as keyvi
+#from test_tools import tmp_dictionary
+import warnings
+
+
+#from keyvi.compiler import (
+#    JsonDictionaryCompiler,
+#    CompletionDictionaryCompiler,
+#    KeyOnlyDictionaryCompiler,
+#    StringDictionaryCompiler,
+#)
+
+
+""" def test_serialization():
+    m = keyvi.Match()
+    m.start = 22
+    m.end = 30
+    m.score = 42
+    d = m.dumps()
+    m2 = keyvi.Match.loads(d)
+    assert m2.start == 22
+    assert m2.end == 30
+    assert m2.score == 42 """
+
+
+""" def test_raw_serialization():
+    c = JsonDictionaryCompiler({"memory_limit_mb": "10"})
+    c.Add("abc", '{"a" : 2}')
+    c.Add("abd", '{"a" : 3}')
+    with tmp_dictionary(c, 'match_object_json.kv') as d:
+        m = d["abc"]
+        assert m.value_as_string() == '{"a":2}'
+        d = m.dumps()
+        m2 = keyvi.Match.loads(d)
+        assert m2.value_as_string() == '{"a":2}'
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            assert m.GetValueAsString() == '{"a":2}'
+            assert len(w) == 1
+            assert issubclass(w[-1].category, DeprecationWarning)
+ """
+
+""" def test_unicode_attributes():
+    m = keyvi.Match()
+    m["küy"] = 22
+    assert m["küy"] == 22
+    m["k2"] = " 吃饭了吗"
+    m.score = 99
+    assert m["k2"] == " 吃饭了吗"
+    assert m.score == 99.0
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        m.SetAttribute("k2", "öäü")
+        assert m["k2"] == "öäü"
+        assert m.GetAttribute("k2") == "öäü"
+        assert len(w) == 2
+        assert issubclass(w[0].category, DeprecationWarning)
+        assert issubclass(w[1].category, DeprecationWarning) """
+
+
+""" def test_bytes_attributes():
+    m = keyvi.Match()
+    bytes_key = bytes(u"äöü".encode('utf-8'))
+    bytes_value = bytes(u"äöüöäü".encode('utf-8'))
+    m[bytes_key] = 22
+    assert m[bytes_key] == 22
+    m["k2"] = bytes_value
+    assert m["k2"] == "äöüöäü"
+
+
+def test_double_attributes():
+    m = keyvi.Match()
+    bytes_key = bytes("abc".encode('utf-8'))
+    m[bytes_key] = 42.0
+    assert m[bytes_key] == 42.0
+
+
+def test_boolean_attributes():
+    m = keyvi.Match()
+    bytes_key = bytes("def".encode('utf-8'))
+    m[bytes_key] = True
+    assert m[bytes_key] == True """
+
+
+def test_start():
+    m = keyvi.Match()
+    m.start = 42
+    assert m.start == 42
+
+
+def test_end():
+    m = keyvi.Match()
+    m.end = 49
+    assert m.end == 49
+
+
+def test_score():
+    m = keyvi.Match()
+    m.score = 149
+    assert m.score == 149
+
+
+""" def test_get_value():
+    c = JsonDictionaryCompiler({"memory_limit_mb": "10"})
+    c.Add("abc", '{"a" : 2}')
+    c.Add("abd", '{"a" : 3}')
+    with tmp_dictionary(c, 'match_object_json.kv') as d:
+        m = d["abc"]
+        assert m.value == {"a": 2}
+        m = d["abd"]
+        assert m.value == {"a": 3} """
+
+
+""" def test_get_value_int():
+    c = CompletionDictionaryCompiler({"memory_limit_mb": "10"})
+    c.Add("abc", 42)
+    c.Add("abd", 21)
+    with tmp_dictionary(c, 'match_object_int.kv') as d:
+        m = d["abc"]
+        assert m.value == 42
+        m = d["abd"]
+        assert m.value == 21
+
+
+def test_get_value_key_only():
+    c = KeyOnlyDictionaryCompiler({"memory_limit_mb": "10"})
+    c.Add("abc")
+    c.Add("abd")
+    with tmp_dictionary(c, 'match_object_key_only.kv') as d:
+        m = d["abc"]
+        assert m.value == ''
+        m = d["abd"]
+        assert m.value == ''
+
+
+def test_get_value_string():
+    c = StringDictionaryCompiler({"memory_limit_mb": "10"})
+    c.Add("abc", "aaaaa")
+    c.Add("abd", "bbbbb")
+    with tmp_dictionary(c, 'match_object_string.kv') as d:
+        m = d["abc"]
+        assert m.value == "aaaaa"
+        m = d["abd"]
+        assert m.value == "bbbbb"
+ """
+
+def test_matched_string():
+    m = keyvi.Match()
+    m.matched_string = "match"
+    assert m.matched_string == "match"
+
+
+def test_bool_operator():
+    m = keyvi.Match()
+    assert not m
+    m.end = 42
+    assert not m is False
+    assert m
