97d8c2ecedd4f78361a840a3a4170869199cc1f7
Hendrik Muhs <hendrik.muhs@gmail.com>
2024-10-20 21:57:54 +0200

    update docker images, FindSnappy

--- cmake_modules/FindSnappy.cmake
+++ cmake_modules/FindSnappy.cmake
@@ -1,16 +1,16 @@
 # Snappy, a fast compressor/decompressor
 
-include(LibFindMacros)
+find_path(Snappy_INCLUDE_DIR NAMES snappy.h)
 
-find_path(Snappy_INCLUDE_DIR
-	NAMES snappy.h
+find_library(Snappy_LIBRARY NAMES snappy)
+
+include(SelectLibraryConfigurations)
+SELECT_LIBRARY_CONFIGURATIONS(SNAPPY)
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+    Snappy DEFAULT_MSG
+    Snappy_LIBRARY Snappy_INCLUDE_DIR
 )
 
-find_library(Snappy_LIBRARY
-	NAMES snappy
-)
-
-set(Snappy_PROCESS_INCLUDES Snappy_INCLUDE_DIR)
-set(Snappy_PROCESS_LIBS Snappy_LIBRARY)
-
-libfind_process(Snappy)
+mark_as_advanced(Snappy_INCLUDE_DIR Snappy_LIBRARY)
--- docker/manylinux-builder/build_and_push.sh
+++ docker/manylinux-builder/build_and_push.sh
@@ -2,14 +2,14 @@
 
 set -euxo pipefail
 
-docker build . \
-    -f Dockerfile \
-    --build-arg base_image="quay.io/pypa/manylinux2014_x86_64" \
-    -t keyvidev/manylinux-builder-x86_64
+#docker build . \
+#    -f Dockerfile \
+#    --build-arg base_image="quay.io/pypa/manylinux_2_28_x86_64" \
+#    -t keyvidev/manylinux-builder-x86_64
 
 docker build . \
     -f Dockerfile \
-    --build-arg base_image="quay.io/pypa/manylinux2014_aarch64" \
+    --build-arg base_image="quay.io/pypa/manylinux_2_28_aarch64" \
     -t keyvidev/manylinux-builder-aarch64
 
 docker build . \
