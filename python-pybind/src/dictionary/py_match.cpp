bab9d0724f2b5bc0aa29d1e8308643903487e203
Hendrik Muhs <hendrik.muhs@gmail.com>
2024-10-27 20:16:35 +0100

    add more match bindings

--- python-pybind/src/dictionary/py_match.cpp
+++ python-pybind/src/dictionary/py_match.cpp
@@ -27,6 +27,8 @@
 namespace py = pybind11;
 namespace kd = keyvi::dictionary;
 
+py::module_ msgpack_ = py::module_::import("msgpack");
+
 void init_keyvi_match(const py::module_ &m) {
   py::class_<kd::Match, std::shared_ptr<kd::Match>>(m, "Match")
       .def(py::init<>())
@@ -34,5 +36,19 @@ void init_keyvi_match(const py::module_ &m) {
       .def_property("end", &kd::Match::GetEnd, &kd::Match::SetEnd)
       .def_property("score", &kd::Match::GetScore, &kd::Match::SetScore)
       .def_property("matched_string", &kd::Match::GetMatchedString, &kd::Match::SetMatchedString)
+      .def_property_readonly("value",
+                             [](const kd::Match &m) -> py::object {
+                               auto packed_value = m.GetMsgPackedValueAsString();
+                               if (packed_value.empty()) {
+                                 return py::none();
+                               }
+                               return msgpack_.attr("loads")(packed_value);
+                             })
+      .def("value_as_string", &kd::Match::GetValueAsString)
+      .def("raw_value_as_string", &kd::Match::GetRawValueAsString)
+      .def("__get_item__", &kd::Match::GetAttributePy)
+      // __setitem__
+      // dumps loads
+      .def_property_readonly("weight", &kd::Match::GetWeight)
       .def("__bool__", [](const kd::Match &m) -> bool { return !m.IsEmpty(); });
 }
