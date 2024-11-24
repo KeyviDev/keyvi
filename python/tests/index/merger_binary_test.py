dd71bc1d7fb2425c1bc88676f7cc0a11af77940a
Hendrik Muhs <hendrik.muhs@gmail.com>
2024-11-24 11:01:11 +0100

    fix test errors

--- python/tests/index/merger_binary_test.py
+++ python/tests/index/merger_binary_test.py
@@ -10,5 +10,5 @@ import os
 
 def test_merger_binary():
     cmd = get_interpreter_executable() + b" " + os.path.join(get_package_root(), b"_pycore" , b"keyvimerger.py") + b" -h"
-    rc = subprocess.call(cmd, shell=True, stdout=open(os.devnull, 'w'))
+    rc = subprocess.call(cmd, shell=True)
     assert rc == 0
