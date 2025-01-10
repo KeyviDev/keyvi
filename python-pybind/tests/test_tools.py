f9cd3dd904656865e4f88aa2ea528592ad99ff67
Hendrik Muhs <hendrik.muhs@gmail.com>
2025-01-10 23:26:34 +0100

    add missing file

---
+++ python-pybind/tests/test_tools.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# some common tools for tests
+
+import contextlib
+import os
+import tempfile
+
+from keyvi_scikit_core.dictionary import Dictionary
+
+
+@contextlib.contextmanager
+def tmp_dictionary(compiler, file_name):
+    tmp_dir = tempfile.gettempdir()
+    fq_file_name = os.path.join(tmp_dir, file_name)
+    compiler.Compile()
+    compiler.WriteToFile(fq_file_name)
+    del compiler
+    d = Dictionary(fq_file_name)
+    yield d
+    del d
+    os.remove(fq_file_name)
