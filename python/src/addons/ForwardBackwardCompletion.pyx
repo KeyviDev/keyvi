6755a04da285f29ce07c93c4e584fd7ba5b284c6
Hendrik Muhs <hendrik.muhs@gmail.com>
2025-01-26 21:38:19 +0100

    rewrite generator

---
+++ python/src/addons/ForwardBackwardCompletion.pyx
@@ -0,0 +1,4 @@
+
+
+    def GetCompletions(self, *args):
+        return call_deprecated_method("GetCompletions", "get_completions", self.get_completions, *args)
---
+++ python/src/addons/StringDictionaryMerger.pyx
@@ -0,0 +1,12 @@
+
+
+    def Add(self, *args):
+        return call_deprecated_method("Add", "add", self.add, *args)
+
+    def Merge(self, *args):
+        return call_deprecated_method("Merge", "merge", self.merge, *args)
+
+    def SetManifest(self, *args):
+        return call_deprecated_method("SetManifest", "set_manifest", self.set_manifest, *args)
+
+    
