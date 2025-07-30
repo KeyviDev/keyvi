e52481b1f5602f7a4d7678f0e6e4d807ff9deb7c
Hendrik Muhs <hendrik.muhs@gmail.com>
2025-07-30 08:45:50 +0200

    implement dumps and loads

--- python-pybind/src/dictionary/py_match.cpp
+++ python-pybind/src/dictionary/py_match.cpp
@@ -19,6 +19,8 @@
 
 #include <memory>
 
+#include "msgpack.hpp"
+
 #include "keyvi/dictionary/dictionary.h"
 #include "keyvi/dictionary/match.h"
 
@@ -53,9 +55,96 @@ void init_keyvi_match(const py::module_ &m) {
                              })
       .def("value_as_string", &kd::Match::GetValueAsString)
       .def("raw_value_as_string", &kd::Match::GetRawValueAsString)
-      .def("__get_item__", &kd::Match::GetAttributePy)
-      // __setitem__
-      // dumps loads
+      .def("__getitem__", [](kd::Match &m, const std::string &key) {
+        return m.GetAttribute(key);
+      })
+      .def("__setitem__", &kd::Match::SetAttribute<std::string>)
+      .def("__setitem__", &kd::Match::SetAttribute<float>)
+      .def("__setitem__", &kd::Match::SetAttribute<int>)
+      .def("__setitem__", &kd::Match::SetAttribute<bool>)
+      .def("dumps",
+           [](const kd::Match &m) -> py::bytes {
+             bool do_pack_rest = false;
+             msgpack::sbuffer msgpack_buffer;
+             msgpack::packer<msgpack::sbuffer> packer(&msgpack_buffer);
+             const double score = m.GetScore();
+             const size_t end = m.GetEnd();
+             const size_t start = m.GetStart();
+             const std::string matched_string = m.GetMatchedString();
+             const std::string raw_value = m.GetRawValueAsString();
+
+             const size_t array_size = score > 0                   ? 5
+                                       : end > 0                   ? 4
+                                       : start > 0                 ? 3
+                                       : matched_string.size() > 0 ? 2
+                                       : raw_value.size() > 0      ? 1
+                                                                   : 0;
+             packer.pack_array(array_size);
+
+             if (array_size > 0) {
+               packer.pack(raw_value);
+             }
+             if (array_size > 1) {
+               packer.pack(matched_string);
+             }
+             if (array_size > 2) {
+               packer.pack(start);
+             }
+             if (array_size > 3) {
+               packer.pack(end);
+             }
+             if (array_size > 4) {
+               packer.pack(score);
+             }
+
+             return py::bytes(msgpack_buffer.data(), msgpack_buffer.size());
+           })
+      .def_static("loads",
+                  [](const std::string_view &serialized_match) -> kd::Match {
+                    kd::Match match;
+                    msgpack::object_handle handle = msgpack::unpack(serialized_match.data(), serialized_match.size());
+                    msgpack::object obj = handle.get();
+
+                    // Ensure it's an array
+                    if (obj.type != msgpack::type::ARRAY) {
+                      throw std::invalid_argument("not a serialized match");
+                    }
+
+                    // Get the array elements
+                    const msgpack::object *array = obj.via.array.ptr;
+                    uint32_t size = obj.via.array.size;
+
+                    if (size > 5) {
+                      throw std::invalid_argument("not a serialized match, unexpected number of elements");
+                    }
+
+                    std::string matched_string, value;
+                    double score;
+                    size_t start, end;
+
+                    try {
+                      switch (size) {
+                        case 5:
+                          array[4].convert(score);
+                          match.SetScore(score);
+                        case 4:
+                          array[3].convert(end);
+                          match.SetEnd(end);
+                        case 3:
+                          array[2].convert(start);
+                          match.SetStart(start);
+                        case 2:
+                          array[1].convert(matched_string);
+                          match.SetMatchedString(matched_string);
+                        case 1:
+                          array[0].convert(value);
+                          match.SetRawValue(value);
+                      }
+                    } catch (const msgpack::type_error &e) {
+                      throw std::invalid_argument("not a serialized match, unexpected element types");
+                    }
+                    return match;
+                  })
       .def_property_readonly("weight", &kd::Match::GetWeight)
       .def("__bool__", [](const kd::Match &m) -> bool { return !m.IsEmpty(); });
 }
--- python-pybind/tests/match_object_test.py
+++ python-pybind/tests/match_object_test.py
@@ -14,7 +14,7 @@ from keyvi2.compiler import (
 )
 
 
-""" def test_serialization():
+def test_serialization():
     m = keyvi.Match()
     m.start = 22
     m.end = 30
@@ -23,27 +23,22 @@ from keyvi2.compiler import (
     m2 = keyvi.Match.loads(d)
     assert m2.start == 22
     assert m2.end == 30
-    assert m2.score == 42 """
+    assert m2.score == 42
 
 
-""" def test_raw_serialization():
+def test_raw_serialization():
     c = JsonDictionaryCompiler({"memory_limit_mb": "10"})
     c.Add("abc", '{"a" : 2}')
     c.Add("abd", '{"a" : 3}')
-    with tmp_dictionary(c, 'match_object_json.kv') as d:
+    with tmp_dictionary(c, "match_object_json.kv") as d:
         m = d["abc"]
         assert m.value_as_string() == '{"a":2}'
         d = m.dumps()
         m2 = keyvi.Match.loads(d)
         assert m2.value_as_string() == '{"a":2}'
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            assert m.GetValueAsString() == '{"a":2}'
-            assert len(w) == 1
-            assert issubclass(w[-1].category, DeprecationWarning)
- """
 
-""" def test_unicode_attributes():
+
+def test_unicode_attributes():
     m = keyvi.Match()
     m["küy"] = 22
     assert m["küy"] == 22
@@ -51,20 +46,12 @@ from keyvi2.compiler import (
     m.score = 99
     assert m["k2"] == " 吃饭了吗"
     assert m.score == 99.0
-    with warnings.catch_warnings(record=True) as w:
-        warnings.simplefilter("always")
-        m.SetAttribute("k2", "öäü")
-        assert m["k2"] == "öäü"
-        assert m.GetAttribute("k2") == "öäü"
-        assert len(w) == 2
-        assert issubclass(w[0].category, DeprecationWarning)
-        assert issubclass(w[1].category, DeprecationWarning) """
 
 
-""" def test_bytes_attributes():
+def test_bytes_attributes():
     m = keyvi.Match()
-    bytes_key = bytes(u"äöü".encode('utf-8'))
-    bytes_value = bytes(u"äöüöäü".encode('utf-8'))
+    bytes_key = bytes("äöü".encode("utf-8"))
+    bytes_value = bytes("äöüöäü".encode("utf-8"))
     m[bytes_key] = 22
     assert m[bytes_key] == 22
     m["k2"] = bytes_value
@@ -73,16 +60,16 @@ from keyvi2.compiler import (
 
 def test_double_attributes():
     m = keyvi.Match()
-    bytes_key = bytes("abc".encode('utf-8'))
+    bytes_key = bytes("abc".encode("utf-8"))
     m[bytes_key] = 42.0
     assert m[bytes_key] == 42.0
 
 
 def test_boolean_attributes():
     m = keyvi.Match()
-    bytes_key = bytes("def".encode('utf-8'))
+    bytes_key = bytes("def".encode("utf-8"))
     m[bytes_key] = True
-    assert m[bytes_key] == True """
+    assert m[bytes_key] == True
 
 
 def test_start():
@@ -118,7 +105,7 @@ def test_get_value_int():
     c = CompletionDictionaryCompiler({"memory_limit_mb": "10"})
     c.Add("abc", 42)
     c.Add("abd", 21)
-    with tmp_dictionary(c, 'match_object_int.kv') as d:
+    with tmp_dictionary(c, "match_object_int.kv") as d:
         m = d["abc"]
         assert m.value == 42
         m = d["abd"]
@@ -136,11 +123,12 @@ def test_get_value_int():
         assert m.value == ''
  """
 
+
 def test_get_value_string():
     c = StringDictionaryCompiler({"memory_limit_mb": "10"})
     c.Add("abc", "aaaaa")
     c.Add("abd", "bbbbb")
-    with tmp_dictionary(c, 'match_object_string.kv') as d:
+    with tmp_dictionary(c, "match_object_string.kv") as d:
         m = d["abc"]
         assert m.value == "aaaaa"
         m = d["abd"]
