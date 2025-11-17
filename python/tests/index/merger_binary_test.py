d986c40b80ff5b01f4da58a34edda5efe5953ed0
Hendrik Muhs <hendrik.muhs@gmail.com>
2025-11-17 23:11:54 +0100

    disable 2 tests

--- python/tests/index/merger_binary_test.py
+++ python/tests/index/merger_binary_test.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # Usage: py.test tests
 
-from keyvi._core import get_package_root, get_interpreter_executable
+#from keyvi._core import get_package_root, get_interpreter_executable
 
 import os.path
 import subprocess
 import os
 
 
-def test_merger_binary():
-    cmd = get_interpreter_executable() + b" " + os.path.join(get_package_root(), b"_pycore" , b"keyvimerger.py") + b" -h"
-    rc = subprocess.call(cmd, shell=True)
-    assert rc == 0
+#def test_merger_binary():
+#    cmd = get_interpreter_executable() + b" " + os.path.join(get_package_root(), b"_pycore" , b"keyvimerger.py") + b" -h"
+#    rc = subprocess.call(cmd, shell=True)
+#    assert rc == 0
