700cf8d5eb717a7cac743759c939598edbc8350c
Hendrik Muhs <hendrik.muhs@gmail.com>
2025-11-18 20:34:30 +0100

    pre-commit again

--- python/tests/index/merger_binary_test.py
+++ python/tests/index/merger_binary_test.py
@@ -4,7 +4,6 @@
 from __future__ import annotations
 
 
-
 # def test_merger_binary():
 #    cmd = get_interpreter_executable() + b" " + os.path.join(get_package_root(), b"_pycore" , b"keyvimerger.py") + b" -h"
 #    rc = subprocess.call(cmd, shell=True)
--- python/tests/utils/jump_consistent_hash_test.py
+++ python/tests/utils/jump_consistent_hash_test.py
@@ -2,11 +2,11 @@
 from __future__ import annotations
 
 
-#from keyvi.util import JumpConsistentHashString
+# from keyvi.util import JumpConsistentHashString
 
-#def test_jump_consistent_hash():
+# def test_jump_consistent_hash():
 #    assert JumpConsistentHashString('some string', 117) == 60
 
-    # test unicode on Python 2 only
+# test unicode on Python 2 only
 #    if sys.version_info[0] == 2:
 #        assert JumpConsistentHashString(u'some string', 117) == 60
